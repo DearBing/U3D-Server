9074f6723679bf26a827711fff3b633e58a244c3
钟兵 <744749338@qq.com>
2020-04-01 22:22:38 +0800

    修改ReadMe，构建.NetCore项目

---
+++ Source/U3DServcer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace U3DServcer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
